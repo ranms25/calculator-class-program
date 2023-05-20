60539da409bb5a640cb27c2fd2693ad1f8900720
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-20 16:16:24 +0800

    Add comment to return values

--- calculator.py
+++ calculator.py
@@ -3,19 +3,19 @@ class Calculator:
 
     # a method for addition
     def add(self, num1, num2):
-        return round(num1 + num2, 2)
+        return round(num1 + num2, 2)    # return value is round off to two decimals
 
     # a method for subtraction
     def subtract(self, num1, num2):
-        return round(num1 - num2, 2)
+        return round(num1 - num2, 2)    # return value is round off to two decimals
     
     # a method for multiplication
     def multiply(self, num1, num2):
-        return round(num1 * num2, 2)
+        return round(num1 * num2, 2)    # return value is round off to two decimals
     
     # a method for division
     def divide(self, num1, num2):
-        if num2 ==0:
-            raise ZeroDivisionError
+        if num2 ==0:                    # raise zero error value for num2
+            raise ZeroDivisionError 
         else:
-            return round(num1 / num2, 2)
+            return round(num1 / num2, 2)    # return value is round off to two decimals
