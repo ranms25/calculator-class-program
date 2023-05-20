bd1ed89bc87b80860d8ff81dee47e180574c001e
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-20 16:07:19 +0800

    Add addition method with round off to two decimals

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
 # Define a class for Calculator
 class Calculator:
-# a method for addition
+
+    # a method for addition
+    def add(self, num1, num2):
+        return round(num1 + num2, 2)
+
 # a method for subtraction
 # a method for multiplication
 # a method for division
