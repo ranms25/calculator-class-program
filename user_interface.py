e151fa816276f7f960a6f3654b0546bca2bd437a
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-20 16:29:10 +0800

    Define user input's first number

--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,8 @@
 # Define class for user interface
 class UserInterface:
+
     # Prompt user to input first number
+    def inputNum1(self):
+        num1 = float(input("Enter first number: "))
 
     # Prompt user to input second number
