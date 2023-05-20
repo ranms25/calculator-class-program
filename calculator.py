3d974d974f98043b74b38f9e7371594bdd88c7b7
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-20 16:59:53 +0800

    Attempt to follow PEP8 python style guide

--- calculator.py
+++ calculator.py
@@ -1,19 +1,19 @@
 # Define a class for Calculator
 class Calculator:
 
-    # a method for addition
+    # This is a method for addition
     def add(self, num1, num2):
         return round(num1 + num2, 2)    # return value is round off to two decimals
 
-    # a method for subtraction
+    # This is a method for subtraction
     def subtract(self, num1, num2):
         return round(num1 - num2, 2)    # return value is round off to two decimals
     
-    # a method for multiplication
+    # This is a method for multiplication
     def multiply(self, num1, num2):
         return round(num1 * num2, 2)    # return value is round off to two decimals
     
-    # a method for division
+    # This is a method for division
     def divide(self, num1, num2):
         if num2 ==0:                    # raise zero error value for num2
             raise ZeroDivisionError 
