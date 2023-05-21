79eeaad72d5c725eac1da57354da6d044eaa54b8
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-22 00:45:43 +0800

    Adjust spacing and Add theme 'vista'

--- calc_gui.py
+++ calc_gui.py
@@ -1,5 +1,6 @@
 # Import modules
 import tkinter as tk
+from tkinter import ttk
 from calculator import Calculator
 from user_validation import UserValidation
 
@@ -8,43 +9,51 @@ class TkinterGUI():
     # Initialize init method
     def __init__(self):
         self.calc = Calculator()
-        self.uv = UserValidation()
-        
-        # Sets window and title
+        self.uv = UserValidation() 
+
+        # Sets window, name, and title
         self.window = tk.Tk()
         self.window.title("Simple Calculator")
-        self.window.geometry("500x400")
+        self.window.geometry("500x350")
+        self.title_label = ttk.Label(self.window, text="MY CALCULATOR", font=('Arial', 16, 'bold'), foreground='purple')
+        self.title_label.pack(pady=15)
+
+        # Create a ttk style and apply the "Adapta" theme
+        style = ttk.Style(self.window)
+        style.theme_use("vista")
         
         # Create a dropdown menu for math operations
         self.operation_var = tk.StringVar(value="Select Operation")     # A StringVar to store selected operations
         operations = ["Addition", "Subtraction", "Multiplication", "Division"]
         self.operation_menu = tk.OptionMenu(self.window, self.operation_var, *operations)
-        self.operation_menu.pack()
+        self.operation_menu.config(width=15, height=1)
+        self.operation_menu.pack(pady=15)
 
         # An input feature for first number
-        self.num1_label = tk.Label(self.window, text="First number:")
+        self.num1_label = ttk.Label(self.window, text="First number:")
         self.num1_label.pack()
-        self.num1_entry = tk.Entry(self.window)
+        self.num1_entry = ttk.Entry(self.window)
         self.num1_entry.pack()
 
         # An input feaure for second number
-        self.num2_label = tk.Label(self.window, text="Second number:")
+        self.num2_label = ttk.Label(self.window, text="Second number:")
         self.num2_label.pack()
-        self.num2_entry = tk.Entry(self.window)
+        self.num2_entry = ttk.Entry(self.window)
         self.num2_entry.pack()    
 
         # A Calculate button
-        self.calculate_button = tk.Button(self.window, text="Calculate", command=self.calculate)
-        self.calculate_button.pack()
+        self.calculate_button = ttk.Button(self.window, text="Calculate", command=self.calculate)
+        self.calculate_button.pack(side=tk.BOTTOM, anchor=tk.N, fill=tk.X)
 
-        # A Clear button 
-        self.calculate_button = tk.Button(self.window, text="Clear", command=self.clear)
-        self.calculate_button.pack()
+        # A Clear button
+        self.clear_button = ttk.Button(self.window, text="Clear", command=self.clear)
+        self.clear_button.pack(side=tk.BOTTOM, anchor=tk.S, fill=tk.X)
 
         # A result feature
-        self.result_label = tk.Label(self.window)
+        self.result_label = ttk.Label(self.window)
         self.result_label.pack()
 
+    # A method that uses imports 
     def calculate(self):
         num1 = self.num1_entry.get()
         num2 = self.num2_entry.get()
@@ -53,17 +62,11 @@ class TkinterGUI():
         try:
             result = self.uv.user_validation(num1, num2, operation)
             if result is not None:
-                self.result_label.config(text=f'\nResult:{result}')
-            else:
-                self.result_label.config(text='Invalid output')
-
+                self.result_label.config(text="\nResult: " + str(result), font=('Verdana', 12, 'bold'))
         except Exception as error:
-            self.result_label.config(text=str(error))
+            self.result_label.config(text=str(error), font=('Helvetica', 10, 'italic'))
 
-            # self.result_label.config(text='Result: {}'.format(result))
-
-
-    # A method that clears the 
+    # A method that clears the inputs and selection of the user
     def clear(self):
         self.operation_var.set("Select Operation") # Reset the operation menu to the initial value
         self.num1_entry.delete(0, tk.END)  # Clear the first number entry
@@ -74,5 +77,3 @@ class TkinterGUI():
 if __name__ == "__main__":
     gui = TkinterGUI()
     gui.window.mainloop()
-
-
--- user_validation.py
+++ user_validation.py
@@ -1,8 +1,9 @@
 # import modules
 from calculator import Calculator
+
 # Define class for user interface
 class UserValidation():
-
+    
     # A method that validates user interaction with the UI
     def user_validation(self, num1, num2, operation):
         self.calc = Calculator()
@@ -14,18 +15,18 @@ class UserValidation():
             # A method that performs calculations based on the selected operation
                 if operation == 'Addition':
                     return self.calc.add(num1, num2)
-                elif operation == 'Subtraction':
+                if operation == 'Subtraction':
                     return self.calc.subtract(num1, num2)
-                elif operation == 'Multiplication':
+                if operation == 'Multiplication':
                     return self.calc.multiply(num1, num2)
-                elif operation == 'Division':
+                if operation == 'Division':
                     return self.calc.divide(num1, num2)
-                else:
-                    raise ValueError('\nPlease choose a math operation!')
+
+            # Create exception to handle value error
             except ValueError:
                 raise ValueError(f"\nCalculator could not recognized \n '{num1}'  or  '{num2}'")        
         else:
             if num1 =='' or num2 =='':
-                raise ValueError('\nIt looks like you did not enter a value.')
+                raise ValueError('\nIt looks like you did not fill all the required values.')
             else:
                 raise ValueError('\nHave you chosen a math operation?')
