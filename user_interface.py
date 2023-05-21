0eea844e84d70dce3c4bb82ee9b7302bff068f6c
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-21 16:14:58 +0800

    Exception for value and zero division error

--- user_interface.py
+++ user_interface.py
@@ -2,8 +2,19 @@
 from calculator import Calculator
 class UserInterface:
 
+    # A method that prompts user choose between math operations
+    def math_operations(self):
+        calc = Calculator()
+
     # A method that prompts user to input two numbers
     def input_nums(self):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        return num1, num2
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        # Exception for value and zero division
+        except ValueError:
+            return None
+        except ZeroDivisionError:
+            return None
+        else:
+            return num1, num2
