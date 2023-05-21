718f17894ac42acc43eef59b4a4d136cd713a320
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-21 17:43:38 +0800

    Start event loop and instance for Tkinter GUI

--- calculator_gui.py
+++ calculator_gui.py
@@ -8,8 +8,18 @@ class TkinterGUI():
     # Initialize init method
     def __init__(self):
         self.calc = Calculator()
-
+        
+        # Sets window and title
         self.window = tk.Tk()
         self.window.title("Simple Calculator")
+     
+    # Start an event loop for GUI
+    def run(self):
+        self.window.mainloop()
+
+# Instance of Tkinter GUI to run the GUI 
+if __name__ == "__main__":
+    gui = TkinterGUI()
+    gui.run()
 
 
