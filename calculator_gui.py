fd105c4fda24d23eebccb7d148190af7baade142
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-21 17:32:41 +0800

    Import user_interface.py and Calculator; Add init method

--- calculator_gui.py
+++ calculator_gui.py
@@ -1,6 +1,15 @@
 # Import modules
 import tkinter as tk
+from calculator import Calculator
+from user_interface import UserInterface
+
 # Define class for GUI using tkinter
 class TkinterGUI():
+    # Initialize init method
+    def __init__(self):
+        self.calc = Calculator()
+
+        self.window = tk.Tk()
+        self.window.title("Simple Calculator")
 
 
