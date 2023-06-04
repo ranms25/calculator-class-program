96fc0ba567637fc4fccd6048fbc3c07b0258be49
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-06-04 15:41:21 +0800

    Multiple renaming / changes to filenames

---
+++ calculator_extended.py
@@ -0,0 +1,17 @@
+# import modules
+from calculator import Calculator
+from user_validation import UserValidation
+
+# inherit Calculator class to Extended Calculator class
+class CalculatorExtended(Calculator, UserValidation):
+    # A NEW method for exponentiation
+    def square(self, num1, num2):
+        return round(num1 ** num2, 2)
+
+    # A validation method for exponentiation
+    def calculate(self, num1, num2, operation):
+        self.validate_user()  # Implement user validation first before calculation by calling validate_user method in UserValidation class
+
+        # A method that will perform calculation if the user choose this operation
+        if operation == 'Exponentiation':
+            return self.square(num1, num2)
