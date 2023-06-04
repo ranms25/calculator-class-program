32b692a115e9264c0445793295e70667cf3e00d6
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-06-04 16:38:47 +0800

    Add calculate method for exponentiation

--- gui_extended.py
+++ gui_extended.py
@@ -4,8 +4,15 @@ from gui import TkinterGUI
 
 # create a class
 class ExtendedGUI(TkinterGUI):
+    # create instantation
     def __init__(self):
         super().__init__()
 
         # Add "Exponentiation" option to the dropdown menu
-        self.operation_menu['menu'].exponent_command(label='Exponentiation', command=lambda: self.operation_var.set('Exponentiation'))
+        self.operation_menu['menu'].add_command(label='Exponentiation', command=lambda: self.operation_var.set('Exponentiation'))
+
+    # A method that obtain user input for num1 and num2
+    def calculate(self):
+        selected_operation = self.operation_var.get()
+        num1 = self.num1_entry.get()
+        num2 = self.num2_entry.get()
