a92351deab4814e20390cce7eda5c95f88a36dae
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-06-04 15:15:31 +0800

    calculator will perform if exponentiation is chosen

--- extended_calculator.py
+++ extended_calculator.py
@@ -4,10 +4,14 @@ from user_validation import UserValidation
 
 # inherit Calculator class to Extended Calculator class
 class ExtendedCalculator(Calculator, UserValidation):
-    # a NEW method for exponentiation
+    # A NEW method for exponentiation
     def square(self, num1, num2):
         return round(num1 ** num2, 2)
 
-    # a validation methof for exponentiation
+    # A validation method for exponentiation
     def calculate(self, num1, num2, operation):
         self.validate_user()  # Implement user validation first before calculation by calling validate_user method in UserValidation class
+
+        # A method that will perform calculation if the user choose this operation
+        if operation == 'Exponentiation':
+            return self.square(num1, num2)
