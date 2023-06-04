6d9868cf3b7eaac92307733fb1468d9c6b5311c1
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-06-04 14:58:05 +0800

    Add square method

--- extended_calculator.py
+++ extended_calculator.py
@@ -3,3 +3,6 @@ from calculator import Calculator
 from user_validation import UserValidation
 
 # inherit Calculator class to Extended Calculator class
+class ExtendedCalculator(Calculator, UserValidation):
+    def square(self, num1, num2):
+        return round(num1 ** num2, 2)
