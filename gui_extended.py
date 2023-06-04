975ddb042da544853194808e783f1f3655d35fd5
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-06-04 18:54:16 +0800

    Overriding validation and user choice

--- gui_extended.py
+++ gui_extended.py
@@ -3,9 +3,7 @@ from gui import TkinterGUI
 from calculator_extended import CalculatorExtended
 from user_validation import UserValidation
 
-
-from calculator_extended import CalculatorExtended
-# create a class
+# Inherit TkinterGUI to ExtendedGUI 
 class ExtendedGUI(TkinterGUI):
     
     # create instantation
@@ -14,22 +12,27 @@ class ExtendedGUI(TkinterGUI):
 
         # Add "Exponentiation" option to the dropdown menu
         self.operation_menu['menu'].add_command(label='Exponentiation', command=lambda: self.operation_var.set('Exponentiation'))
-
+    
     # A method that obtain user input for num1 and num2
     def calculate(self):
         selected_operation = self.operation_var.get()
         num1 = self.num1_entry.get()
-        num2 = self.num2_entry.get()
-
+        num2 = self.num2_entry.get() 
+        
         # Instantiate the UserValidation and CalculatorExtended classes
         self.uv = UserValidation()
         self.ce = CalculatorExtended()
 
         # Try Block to validate user response when they choose exponentiation
         try:
+            self.uv.validate_user(num1, num2, selected_operation)
+            
+            # If user choose exponentiation, the calculation will execute
             if selected_operation == "Exponentiation":
                 result = self.ce.exponent(float(num1), float(num2))
-                self.result_label.config(text="\nResult: " + str(result), font=('Verdana', 12, 'bold'))
+                self.result_label.config(text=f"{num1} raise to {num2} is \n" + str(result), font=('Verdana', 12, 'bold'))
+
+            # If not, it will let the parent class perform the chosen operation and catch exception
             else:
                 super().calculate()
         except Exception as error:
