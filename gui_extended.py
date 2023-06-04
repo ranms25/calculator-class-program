6d5da3c573fdf44f036cb69eb25a4ac5cc2a91f4
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-06-04 16:25:52 +0800

    Add Exponentiation option to the dropdown menu

--- gui_extended.py
+++ gui_extended.py
@@ -1,7 +1,11 @@
 # import necessary module
+from calculator_extended import CalculatorExtended
 from gui import TkinterGUI
 
 # create a class
 class ExtendedGUI(TkinterGUI):
     def __init__(self):
         super().__init__()
+
+        # Add "Exponentiation" option to the dropdown menu
+        self.operation_menu['menu'].exponent_command(label='Exponentiation', command=lambda: self.operation_var.set('Exponentiation'))
---
+++ main.py
@@ -0,0 +1,6 @@
+from gui_extended import ExtendedGUI
+
+# Instance of Tkinter GUI to run the GUI 
+if __name__ == "__main__":
+    gui = ExtendedGUI()
+    gui.window.mainloop()
